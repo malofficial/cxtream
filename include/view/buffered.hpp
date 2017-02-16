efca69830aa910ede6001339d3a5a3fe6ffefaac
Filip Matzner <floop@floop.cz>
2017-02-16 19:18:31 +0100

    Improve and extend unit tests of view::buffered

--- include/view/buffered.hpp
+++ include/view/buffered.hpp
@@ -16,10 +16,10 @@
 #include <range/v3/core.hpp>
 #include <range/v3/view/all.hpp>
 
-#include <iostream>
 
 namespace stream {
   
+
   template<typename Rng>
   struct buffered_view
     : ranges::view_facade<buffered_view<Rng>>
@@ -29,8 +29,8 @@ namespace stream {
 
       Rng rng_;
 
-      std::launch policy_;
       std::size_t n_;
+      std::launch policy_;
 
       struct cursor
       {
@@ -38,8 +38,8 @@ namespace stream {
           buffered_view<Rng> *rng_ = nullptr;
           ranges::range_iterator_t<Rng> it_ = {};
 
-          std::launch policy_;
           std::size_t n_;
+          std::launch policy_;
 
           std::deque<
             std::shared_future<
@@ -54,9 +54,8 @@ namespace stream {
           void fill_buffer()
           {
             while (it_ != ranges::end(rng_->rng_) && buffer_.size() < n_) {
-              auto it_copy = it_;
-              buffer_.emplace_back(std::async(policy_, [it_copy](){
-                return *it_copy;
+              buffer_.emplace_back(std::async(policy_, [it=it_](){
+                return *it;
               }));
               ++it_;
             }
@@ -67,8 +66,8 @@ namespace stream {
           explicit cursor(buffered_view<Rng>& rng)
             : rng_{&rng},
               it_{ranges::begin(rng.rng_)},
-              policy_{rng.policy_},
-              n_{rng.n_}
+              n_{rng.n_},
+              policy_{rng.policy_}
           {
             fill_buffer();
           }
@@ -99,8 +98,8 @@ namespace stream {
 
     public:
       buffered_view() = default;
-      buffered_view(Rng rng, std::launch policy, std::size_t n)
-        : rng_{rng}, policy_{policy}, n_{n}
+      buffered_view(Rng rng, std::size_t n, std::launch policy = std::launch::async)
+        : rng_{rng}, n_{n}, policy_{policy}
       {}
 
       CONCEPT_REQUIRES(ranges::SizedRange<Rng const>())
@@ -116,14 +115,17 @@ namespace stream {
       }
   };
 
+
   namespace view {
+
 
     template<typename Rng, CONCEPT_REQUIRES_(ranges::ForwardRange<Rng>())>
     buffered_view<ranges::view::all_t<Rng>> buffered_view(
-      Rng&& rng, std::launch policy, std::size_t n)
+      Rng&& rng, std::size_t n, std::launch policy = std::launch::async)
     {
-      return {ranges::view::all(std::forward<Rng>(rng)), policy, n};
+      return {ranges::view::all(std::forward<Rng>(rng)), n, policy};
     }
+
 
   } //end namespace view
 } //end namespace stream
--- test/view/buffered.cpp
+++ test/view/buffered.cpp
@@ -11,22 +11,28 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE buffered_view_test
 
-#include <tuple>
 #include <vector>
-#include <ostream>
+#include <memory>
 
 #include <boost/test/unit_test.hpp>
 #include <range/v3/to_container.hpp>
 #include <range/v3/view/iota.hpp>
-#include <range/v3/view/generate_n.hpp>
-#include <range/v3/view/move.hpp>
-#include <range/v3/view/reverse.hpp>
-#include <range/v3/view/zip.hpp>
+#include <range/v3/view/indirect.hpp>
+
 #include <view/buffered.hpp>
 
 
 using namespace stream;
 using namespace boost;
+using namespace std::chrono_literals;
+
+
+void test_use_count(const std::vector<std::shared_ptr<int>>& ptrs,
+                    const std::vector<int>& desired)
+{
+  for (unsigned i = 0; i < ptrs.size(); ++i)
+    BOOST_TEST(ptrs[i].use_count() == desired[i]);
+}
 
 
 BOOST_AUTO_TEST_CASE(buffered_view_test)
@@ -34,7 +40,7 @@ BOOST_AUTO_TEST_CASE(buffered_view_test)
   {
     // check simple traverse
     std::vector<int> data = {1, 2, 3, 4, 5};
-    auto rng = view::buffered_view(data, std::launch::async, 2);
+    auto rng = view::buffered_view(data, 2);
     auto it = ranges::begin(rng);
     static_assert(std::is_same<int, decltype(*it)>{});
     BOOST_TEST(*it == 1);
@@ -43,5 +49,54 @@ BOOST_AUTO_TEST_CASE(buffered_view_test)
 
     BOOST_TEST(*it == 1);
     BOOST_TEST(generated == data, test_tools::per_element{});
+  }
+
+  {
+    // check if it is really buffered
+    std::vector<std::shared_ptr<int>> data;
+    for (int i = 0; i < 5; ++i)
+      data.emplace_back(std::make_shared<int>(i));
+    auto rng = view::buffered_view(data, 2);
+
+    // iterate through and check not-yet visited elements' use count
+    test_use_count(data, {1, 1, 1, 1, 1});
+    auto it = ranges::begin(rng);
+    BOOST_CHECK(it != ranges::end(rng));
+    std::this_thread::sleep_for(20ms);
+    test_use_count(data, {2, 2, 1, 1, 1});
+    ++it;
+    std::this_thread::sleep_for(20ms);
+    test_use_count(data, {1, 2, 2, 1, 1});
+    ++it;
+    std::this_thread::sleep_for(20ms);
+    test_use_count(data, {1, 1, 2, 2, 1});
+    ++it;
+    std::this_thread::sleep_for(20ms);
+    test_use_count(data, {1, 1, 1, 2, 2});
+    ++it;
+    BOOST_CHECK(it != ranges::end(rng));
+    std::this_thread::sleep_for(20ms);
+    test_use_count(data, {1, 1, 1, 1, 2});
+    ++it;
+    BOOST_CHECK(it == ranges::end(rng));
+    std::this_thread::sleep_for(20ms);
+    test_use_count(data, {1, 1, 1, 1, 1});
+
+    // iterate with two iterators at once
+    auto it2 = ranges::begin(rng);
+    std::this_thread::sleep_for(20ms);
+    test_use_count(data, {2, 2, 1, 1, 1});
+    auto it3 = ranges::begin(rng);
+    std::this_thread::sleep_for(20ms);
+    test_use_count(data, {3, 3, 1, 1, 1});
+    ++it2;
+    std::this_thread::sleep_for(20ms);
+    test_use_count(data, {2, 3, 2, 1, 1});
+    static_assert(std::is_same<std::shared_ptr<int>, decltype(*it)>{});
+
+    // check values
+    auto vals = rng | ranges::view::indirect | ranges::to_vector;
+    auto desired = ranges::view::iota(0, 5) | ranges::to_vector;
+    BOOST_TEST(vals == desired, test_tools::per_element{});
   }
 }
