8bd082d62c9712998d54a5d9a7b0a976569c93e3
Filip Matzner <floop@floop.cz>
2017-03-02 22:30:51 +0100

    Add batch_view.

---
+++ include/view/batch.hpp
@@ -0,0 +1,206 @@
+/// \file
+// Stream prototype library
+//
+//  Copyright Filip Matzner 2017
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0.
+//  (see http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef STREAM_BATCH_VIEW_HPP
+#define STREAM_BATCH_VIEW_HPP
+
+#include <deque>
+#include <future>
+#include <climits>
+#include <range/v3/core.hpp>
+#include <range/v3/view/all.hpp>
+#include <utility/tuple.hpp>
+
+
+namespace stream {
+  
+
+  /* batch_size */
+
+
+  template<typename Tuple>
+  constexpr bool is_same_batch_size(const Tuple& tuple)
+  {
+    bool same = true;
+    if (std::tuple_size<Tuple>{} > 0) {
+      auto bs = std::get<0>(tuple).value.size();
+      utility::tuple_for_each([bs, &same](const auto& column){
+        same &= (column.value.size() == bs);
+      }, tuple);
+    }
+    return same;
+  }
+
+  template<typename Tuple>
+  constexpr std::size_t batch_size(const Tuple& tuple)
+  {
+    static_assert(std::tuple_size<Tuple>{} &&
+      "Cannot get batch size if there are no columns");
+    assert(is_same_batch_size(tuple) &&
+      "All the columns have to have equal batch size");
+    return std::get<0>(tuple).value.size();
+  }
+
+
+  /* batch_view */
+
+
+  template<typename Rng>
+  struct batch_view
+    : ranges::view_facade<batch_view<Rng>>
+  {
+    private:
+      friend ranges::range_access;
+
+      Rng rng_;
+      std::size_t n_;
+
+      struct cursor
+      {
+        private:
+          batch_view<Rng> *rng_ = nullptr;
+          ranges::range_iterator_t<Rng> it_ = {};
+
+          using batch_t_ = ranges::range_value_t<Rng>;
+          using column_idxs_t = std::make_index_sequence<std::tuple_size<batch_t_>{}>;
+          // the batch will be a pointer to allow moving from it in const functions
+          std::shared_ptr<batch_t_> batch_ = std::make_shared<batch_t_>();
+
+          std::size_t subbatch_idx_ = 0;
+          std::shared_ptr<batch_t_> subbatch_;
+
+          bool done_ = false;
+
+
+          // move i-th element from subbatch_ to batch_
+          template<std::size_t... Is>
+          void move_from_subbatch(std::size_t i, std::index_sequence<Is...>)
+          {
+            (..., (std::get<Is>(*batch_).value.push_back(
+              std::move(std::get<Is>(*subbatch_).value[i]))));
+          }
+
+
+          // find the first non-empty subbatch and return if successful
+          bool find_next()
+          {
+            // do nothing if the end of iteration is reached
+            if (subbatch_idx_ == batch_size(*subbatch_) &&
+                it_ == ranges::end(rng_->rng_)) {
+              return false;
+            }
+            // otherwise find the first non-empty subbatch
+            while (subbatch_idx_ == batch_size(*subbatch_)) {
+              if (++it_ == ranges::end(rng_->rng_)) {
+                return false;
+              }
+              subbatch_ = std::make_shared<batch_t_>(*it_);
+              subbatch_idx_ = 0;
+            }
+            return true;
+          }
+          
+
+          // fill the batch_ with the elements from the current subbatch_
+          void fill_batch()
+          {
+            do {
+              move_from_subbatch(subbatch_idx_++, column_idxs_t{});
+            } while (batch_size(*batch_) < rng_->n_ && find_next());
+          }
+
+
+        public:
+          cursor() = default;
+          explicit cursor(batch_view<Rng>& rng)
+            : rng_{&rng},
+              it_{ranges::begin(rng_->rng_)}
+          {
+            static_assert(std::tuple_size<std::decay_t<decltype(*batch_)>>{} &&
+              "The range to be batched has to contain at least one column");
+            // do nothing if the subrange is empty
+            if (it_ != ranges::end(rng_->rng_)) {
+              subbatch_ = std::make_shared<batch_t_>(*it_);
+              // if the first subbatch is empty, try to find the next non-empty one
+              if (batch_size(*subbatch_) == 0) {
+                next();
+              } else {
+                fill_batch();
+              }
+            } else {
+              done_ = true;
+            }
+          }
+          decltype(auto) read() const
+          {
+            return *batch_;
+          }
+          bool equal(ranges::default_sentinel) const
+          {
+            return done_;
+          }
+          bool equal(const cursor& that) const
+          {
+            assert(rng_ == that.rng_);
+            return it_ == that.it_ && subbatch_idx_ == that.subbatch_idx_;
+          }
+          void next()
+          {
+            batch_ = std::make_shared<batch_t_>();
+            if (find_next()) {
+              fill_batch();
+            } else {
+              done_ = true;
+            }
+          }
+      };
+
+      cursor begin_cursor()
+      {
+        return cursor{*this};
+      }
+
+    public:
+      batch_view() = default;
+      batch_view(Rng rng, std::size_t n)
+        : rng_{rng}, n_{n}
+      {}
+  };
+
+
+  class batch_fn {
+
+    private:
+
+      friend ranges::view::view_access;
+
+      static auto bind(
+        batch_fn batch,
+        std::size_t n)
+      {
+        return ranges::make_pipeable(std::bind(batch, std::placeholders::_1, n));
+      }
+
+    public:
+
+      template<typename Rng, CONCEPT_REQUIRES_(ranges::InputRange<Rng>())>
+      batch_view<ranges::view::all_t<Rng>> operator()(
+        Rng&& rng,
+        std::size_t n) const
+      {
+        return {ranges::view::all(std::forward<Rng>(rng)), n};
+      }
+
+  };
+
+  constexpr ranges::view::view<batch_fn> batch{};
+
+} //end namespace stream
+#endif
---
+++ test/view/batch.cpp
@@ -0,0 +1,172 @@
+/// \file
+// Stream prototype library
+//
+//  Copyright Filip Matzner 2017
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0.
+//  (see http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE batch_view_test
+
+#include <vector>
+#include <memory>
+
+#include <boost/test/unit_test.hpp>
+#include <range/v3/to_container.hpp>
+#include <range/v3/view/iota.hpp>
+#include <range/v3/view/move.hpp>
+#include <range/v3/view/indirect.hpp>
+
+#include <view/batch.hpp>
+#include <column_transform.hpp>
+
+
+using namespace stream;
+using namespace boost;
+
+
+inline namespace columns {
+  STREAM_DEFINE_COLUMN(A, int)
+  STREAM_DEFINE_COLUMN(B, double)
+  STREAM_DEFINE_COLUMN(Unique, std::unique_ptr<int>)
+  STREAM_DEFINE_COLUMN(Shared, std::shared_ptr<int>)
+}
+
+
+auto generate_batched_data(std::vector<int> batch_sizes)
+{
+  std::vector<std::tuple<Unique, Shared>> data;
+  int counter = 0;
+  for (int i = 0; i < batch_sizes.size(); ++i) {
+    std::vector<std::unique_ptr<int>> unique_data;
+    std::vector<std::shared_ptr<int>> shared_data;
+    for (int j = 0; j < batch_sizes[i]; ++j) {
+      unique_data.emplace_back(std::make_unique<int>(counter));
+      shared_data.emplace_back(std::make_shared<int>(counter));
+      ++counter;
+    }
+    data.emplace_back(std::make_tuple(std::move(unique_data),
+                                      std::move(shared_data)));
+  }
+  return data;
+}
+
+
+auto generate_regular_batched_data(int batches, int batch_size)
+{
+  return generate_batched_data(std::vector<int>(batches, batch_size));
+}
+
+
+template<typename Data>
+void check_20_elems_batch_size_2(Data data)
+{
+    auto rng = data | view::move | batch(3);
+    using tuple_type = decltype(*ranges::begin(rng));
+    static_assert(std::is_same<std::tuple<Unique, Shared>&, tuple_type>{});
+    using first_type = decltype(std::get<0>(*ranges::begin(rng)).value[0]);
+    static_assert(std::is_same<std::unique_ptr<int>&, first_type>{});
+    using second_type = decltype(std::get<1>(*ranges::begin(rng)).value[0]);
+    static_assert(std::is_same<std::shared_ptr<int>&, second_type>{});
+
+    // iterate through batches
+    std::vector<int> result_unique;
+    std::vector<int> result_shared;
+    int batch_n = 0;
+    int n = 0;
+    for (auto&& tuple : rng) {
+      // the last batch should be smaller
+      if (batch_n == 6) {
+        BOOST_TEST(std::get<0>(tuple).value.size() == 2);
+        BOOST_TEST(std::get<1>(tuple).value.size() == 2);
+      }
+      else {
+        BOOST_TEST(std::get<0>(tuple).value.size() == 3);
+        BOOST_TEST(std::get<1>(tuple).value.size() == 3);
+      }
+      BOOST_CHECK(is_same_batch_size(tuple));
+
+      // iterate through batch values
+      for (auto& elem : std::get<0>(tuple).value) {
+        result_unique.push_back(*elem);
+        ++n;
+      }
+      for (auto& elem : std::get<1>(tuple).value) {
+        result_shared.push_back(*elem);
+      }
+      ++batch_n;
+    }
+    BOOST_TEST(batch_n == 7);
+    BOOST_TEST(n == 20);
+
+    auto desired = ranges::view::iota(0, 20) | ranges::to_vector;
+    BOOST_TEST(result_unique == desired, test_tools::per_element{});
+    BOOST_TEST(result_shared == desired, test_tools::per_element{});
+}
+
+
+BOOST_AUTO_TEST_CASE(batch_view_test)
+{
+  {
+    // batch out of larger batches
+    auto data = generate_regular_batched_data(3, 4);
+
+    auto rng = data | view::move | batch(1);
+    using tuple_type = decltype(*ranges::begin(rng));
+    static_assert(std::is_same<std::tuple<Unique, Shared>&, tuple_type>{});
+    using first_type = decltype(std::get<0>(*ranges::begin(rng)).value[0]);
+    static_assert(std::is_same<std::unique_ptr<int>&, first_type>{});
+    using second_type = decltype(std::get<1>(*ranges::begin(rng)).value[0]);
+    static_assert(std::is_same<std::shared_ptr<int>&, second_type>{});
+
+    // iterate through batches
+    std::vector<int> result_unique;
+    std::vector<int> result_shared;
+    int n = 0;
+    for (auto&& tuple : rng) {
+      // the batch should be only a single element
+      BOOST_TEST(std::get<0>(tuple).value.size() == 1);
+      BOOST_TEST(std::get<1>(tuple).value.size() == 1);
+      // remember the values
+      result_unique.push_back(*(std::get<0>(tuple).value[0]));
+      result_shared.push_back(*(std::get<1>(tuple).value[0]));
+      ++n;
+    }
+    BOOST_TEST(n == 12);
+
+    auto desired = ranges::view::iota(0, 12) | ranges::to_vector;
+    BOOST_TEST(result_unique == desired, test_tools::per_element{});
+    BOOST_TEST(result_shared == desired, test_tools::per_element{});
+  }
+
+  {
+    // batch out of smaller batches
+    check_20_elems_batch_size_2(generate_regular_batched_data(10, 2));
+  }
+
+  {
+    // batch out of iregularly sized batches
+    check_20_elems_batch_size_2(generate_batched_data({0, 1, 2, 0, 5, 2, 0, 1, 7, 0, 2, 0, 0}));
+  }
+
+  {
+    // batch out of empty batches
+    auto data = generate_batched_data({0, 0, 0, 0});
+
+    auto rng = data | view::move | batch(1);
+
+    BOOST_CHECK(rng.begin() == rng.end());
+  }
+
+  {
+    // batch out of empty range
+    auto data = generate_batched_data({});
+
+    auto rng = data | view::move | batch(1);
+
+    BOOST_CHECK(rng.begin() == rng.end());
+  }
+}
