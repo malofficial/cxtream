2a6abb22d55340b10b30ad3a65e5c1e103ca5721
Filip Matzner <floop@floop.cz>
2017-03-27 10:32:55 +0200

    Refactor base64.hpp

--- include/cxtream/core/base64.hpp
+++ include/cxtream/core/base64.hpp
@@ -11,22 +11,19 @@
 #ifndef CXTREAM_CORE_BASE64_HPP
 #define CXTREAM_CORE_BASE64_HPP
 
+#include <boost/algorithm/string.hpp>
+#include <boost/archive/iterators/base64_from_binary.hpp>
+#include <boost/archive/iterators/binary_from_base64.hpp>
+#include <boost/archive/iterators/transform_width.hpp>
+
 #include <vector>
 #include <string>
 
-#include <boost/archive/iterators/binary_from_base64.hpp>
-#include <boost/archive/iterators/base64_from_binary.hpp>
-#include <boost/archive/iterators/transform_width.hpp>
-#include <boost/algorithm/string.hpp>
-
 namespace cxtream {
 
-
-  /* decode base64 string */
-
-
-  std::vector<std::uint8_t> base64_decode(const std::string& b64data)
-  {
+/// Decode base64 encoded string to a vector of bytes.
+std::vector<std::uint8_t> base64_decode(const std::string& b64data)
+{
     using namespace boost::archive::iterators;
     using It = transform_width<binary_from_base64<std::string::const_iterator>, 8, 6>;
 
@@ -35,20 +32,16 @@ namespace cxtream {
     while (len && b64data[len - 1] == '=') --len;
 
     return std::vector<std::uint8_t>(It(std::begin(b64data)), It(std::begin(b64data) + len));
-  }
+}
 
-
-  /* encode base64 string */
-
-
-  std::string base64_encode(const std::vector<std::uint8_t>& data)
-  {
+/// Encode a vector of bytes to a base64 encoded string.
+std::string base64_encode(const std::vector<std::uint8_t>& data)
+{
     using namespace boost::archive::iterators;
     using It = base64_from_binary<transform_width<std::vector<std::uint8_t>::const_iterator, 6, 8>>;
     std::string res(It(std::begin(data)), It(std::end(data)));
     return res.append((3 - data.size() % 3) % 3, '=');
-  }
-
+}
 
 } // end namespace cxtream
 #endif
