8f42c13b075af004e5a18ea95927c43a84587512
Filip Matzner <floop@floop.cz>
2017-03-29 17:10:03 +0200

    Refactor drop.hpp

--- include/cxtream/core/drop.hpp
+++ include/cxtream/core/drop.hpp
@@ -11,31 +11,33 @@
 #ifndef CXTREAM_CORE_DROP_HPP
 #define CXTREAM_CORE_DROP_HPP
 
-#include <range/v3/view/transform.hpp>
-
 #include <cxtream/core/utility/tuple.hpp>
+
+#include <range/v3/view/transform.hpp>
 
 namespace cxtream {
 
-
-  /* drop */
-
-
-  template<typename... Columns>
-  constexpr auto drop_fn()
-  {
+template<typename... Columns>
+constexpr auto drop_fn()
+{
     static_assert(sizeof...(Columns) == 1, "Dropping multiple columns simultaneously is not"
-      " implemented yet. Please drop the columns one by one.");
+                                           " implemented yet. Please drop the columns one by one.");
 
     return ranges::view::transform([](auto&& source) {
-      return utility::tuple_remove<Columns...>(std::forward<decltype(source)>(source));
+        return utility::tuple_remove<Columns...>(std::forward<decltype(source)>(source));
     });
-  }
+}
 
-  // allow calls without parentheses
-  template<typename... Columns>
-  auto drop = drop_fn<Columns...>();
+/// Drops columns from a range of tuples of columns.
+///
+/// Example:
+/// \code
+///     CXTREAM_DEFINE_COLUMN(id, int)
+///     auto rng = ranges::view::iota(0, 10) | cxtream::create<id> | cxtream::drop<id>;
+///     // rng is a range of empty tuples
+/// \endcode
+template <typename... Columns>
+auto drop = drop_fn<Columns...>();
 
-
-} // end namespace cxtream
+}  // end namespace cxtream
 #endif
