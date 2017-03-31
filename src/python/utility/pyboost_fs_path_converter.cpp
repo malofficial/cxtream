aac8edca2592d912f08e9bd5126184e2ec1ebd35
Filip Matzner <floop@floop.cz>
2017-03-31 17:17:01 +0200

    Refactor pyboost_fs_path_converter.cpp

--- src/python/utility/pyboost_fs_path_converter.cpp
+++ src/python/utility/pyboost_fs_path_converter.cpp
@@ -8,54 +8,44 @@
  *  license agreement.
  *********************************************************/
 
-#include <experimental/filesystem>
+#include <cxtream/python/utility/pyboost_fs_path_converter.hpp>
 
 #include <boost/python.hpp>
 
-#include <cxtream/python/utility/pyboost_fs_path_converter.hpp>
+#include <experimental/filesystem>
 
 namespace cxtream::python::utility {
 
-  namespace {
-    namespace fs = std::experimental::filesystem;
-    namespace py = boost::python;
-  }
+PyObject* fs_path_to_python_str::convert(const std::experimental::filesystem::path& path)
+{
+    return boost::python::incref(boost::python::object(path.string()).ptr());
+}
 
-
-  PyObject* fs_path_to_python_str::convert(const fs::path& path)
-  {
-    return py::incref(py::object(path.string()).ptr());
-  }
-
-
-  fs_path_from_python_str::fs_path_from_python_str()
-  {
+fs_path_from_python_str::fs_path_from_python_str()
+{
     boost::python::converter::registry::push_back(
       &convertible,
       &construct,
-      boost::python::type_id<fs::path>());
-  }
+      boost::python::type_id<std::experimental::filesystem::path>());
+}
 
-
-  void* fs_path_from_python_str::convertible(PyObject* obj_ptr)
-  {
+void* fs_path_from_python_str::convertible(PyObject* obj_ptr)
+{
     if (!PyUnicode_Check(obj_ptr)) return 0;
     return obj_ptr;
-  }
+}
 
+void fs_path_from_python_str::construct(
+  PyObject* obj_ptr,
+  boost::python::converter::rvalue_from_python_stage1_data* data)
+{
+      const char* value = PyUnicode_AsUTF8(obj_ptr);
+      if (value == 0) boost::python::throw_error_already_set();
+      void* storage = ((boost::python::converter::rvalue_from_python_storage<
+                         std::experimental::filesystem::path>*)data)
+                        ->storage.bytes;
+      new (storage) std::experimental::filesystem::path(value);
+      data->convertible = storage;
+}
 
-  void fs_path_from_python_str::construct(
-    PyObject* obj_ptr,
-    boost::python::converter::rvalue_from_python_stage1_data* data)
-  {
-    const char* value = PyUnicode_AsUTF8(obj_ptr);
-    if (value == 0) boost::python::throw_error_already_set();
-    void* storage = (
-      (boost::python::converter::rvalue_from_python_storage<fs::path>*)
-        data)->storage.bytes;
-    new (storage) fs::path(value);
-    data->convertible = storage;
-  }
-
-
-} //end namespace cxtream::python::utility
+} // namespace cxtream::python::utility
