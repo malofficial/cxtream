035341a3aed6f3b73dd6df5e6b5e422f20ef31c6
Filip Matzner <floop@floop.cz>
2017-03-31 14:38:15 +0200

    Refactor pyboost_fs_path_converter.hpp

--- include/cxtream/python/utility/pyboost_fs_path_converter.hpp
+++ include/cxtream/python/utility/pyboost_fs_path_converter.hpp
@@ -11,35 +11,24 @@
 #ifndef CXTREAM_PYTHON_UTILITY_PYBOOST_FS_PATH_CONVERTER_HPP
 #define CXTREAM_PYTHON_UTILITY_PYBOOST_FS_PATH_CONVERTER_HPP
 
-#include <experimental/filesystem>
-
 #include <boost/python.hpp>
+
+#include <experimental/filesystem>
 
 namespace cxtream::python::utility {
 
-  namespace {
-    namespace fs = std::experimental::filesystem;
-    namespace py = boost::python;
-  }
+struct fs_path_to_python_str {
+    static PyObject* convert(const std::experimental::filesystem::path& path);
+};
 
-
-  struct fs_path_to_python_str
-  {
-    static PyObject* convert(const fs::path& path);
-  };
-
-
-  struct fs_path_from_python_str
-  {
+struct fs_path_from_python_str {
     fs_path_from_python_str();
 
     static void* convertible(PyObject* obj_ptr);
 
-    static void construct(
-      PyObject* obj_ptr,
-      py::converter::rvalue_from_python_stage1_data* data);
-  };
+    static void construct(PyObject* obj_ptr,
+                          boost::python::converter::rvalue_from_python_stage1_data* data);
+};
 
-
-} //end namespace cxtream::python::utility
+}  // namespace cxtream::python::utility
 #endif
