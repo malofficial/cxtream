dba78a7b284e2ebeb7edc9caee4411c737a82c4d
Filip Matzner <floop@floop.cz>
2017-03-31 17:47:11 +0200

    Refactor test transform.cpp

--- test/core/for_each.cpp
+++ test/core/for_each.cpp
@@ -35,7 +35,7 @@ BOOST_AUTO_TEST_CASE(test_partial_for_each)
   
     view::iota(1, 5)
       | view::transform(std::make_tuple<int>)
-      | partial_for_each(from<int>, [&generated](const std::tuple<int> &t){
+      | partial_for_each(from<int>, [&generated](const std::tuple<int> &t) {
             generated.push_back(std::get<0>(t));
             return 42;
         })
@@ -50,11 +50,11 @@ BOOST_AUTO_TEST_CASE(test_partial_for_each_move_only)
     std::vector<int> generated;
   
     view::iota(1, 5)
-      | view::transform([](int i){
+      | view::transform([](int i) {
             return std::make_tuple(std::make_unique<int>(i));
         })
       | partial_for_each(from<std::unique_ptr<int>>,
-          [&generated](const std::tuple<std::unique_ptr<int>&>& t){
+          [&generated](const std::tuple<std::unique_ptr<int>&>& t) {
               generated.push_back(*std::get<0>(t));
         })
       | to_vector;
@@ -83,7 +83,7 @@ BOOST_AUTO_TEST_CASE(test_for_each_move_only)
     data
       | view::move
       | for_each(from<Int, Unique>,
-          [&generated](const int& v, const std::unique_ptr<int>& p){
+          [&generated](const int& v, const std::unique_ptr<int>& p) {
               generated.push_back(v + *p);
         })
       | to_vector;
--- test/core/transform.cpp
+++ test/core/transform.cpp
@@ -11,9 +11,10 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE transform_test
 
-#include <memory>
-#include <tuple>
-#include <vector>
+#include "common.hpp"
+
+#include <cxtream/core/drop.hpp>
+#include <cxtream/core/transform.hpp>
 
 #include <boost/test/unit_test.hpp>
 #include <range/v3/to_container.hpp>
@@ -21,113 +22,93 @@
 #include <range/v3/view/move.hpp>
 #include <range/v3/view/zip.hpp>
 
-#include <cxtream/core/drop.hpp>
-#include <cxtream/core/transform.hpp>
-
-#include "common.hpp"
+#include <memory>
+#include <tuple>
+#include <vector>
 
 using namespace cxtream;
 using namespace ranges;
 using namespace boost;
 
-
 BOOST_AUTO_TEST_CASE(test_partial_transform)
 {
-  // partial_transform (int){'f', ..., 'i'} to (char){'a', ..., 'd'}
-  // the char column is appended
-  auto data =
-      view::iota((int)'f', (int)'j')
-    | view::transform(std::make_tuple<int>)
-    | partial_transform(from<int>, to<char>, [](std::tuple<int> t){
-        return std::make_tuple((char)(std::get<0>(t) - 5));
-      });
+    // partial_transform (int){'f', ..., 'i'} to (char){'a', ..., 'd'}
+    // the char column is appended
+    auto data = view::iota((int)'f', (int)'j')
+      | view::transform(std::make_tuple<int>)
+      | partial_transform(from<int>, to<char>, [](std::tuple<int> t) {
+            return std::make_tuple((char)(std::get<0>(t) - 5));
+        });
 
-  auto desired =
+    auto desired =
       view::zip(view::iota((char)'a', (char)'e'),
                 view::iota((int)'f', (int)'j'))
-    | view::transform([](auto t){ return std::tuple<char, int>(t); });
+      | view::transform([](auto t) { return std::tuple<char, int>(t); });
 
-  test_ranges_equal(data, desired);
+    test_ranges_equal(data, desired);
 }
-
 
 BOOST_AUTO_TEST_CASE(test_to_itself)
 {
-  // transform a single column to itself
-  std::vector<std::tuple<Int, Double>> data = {{{3},{5.}}, {{1},{2.}}};
-
-  auto generated =
-      data
-    | transform(from<Int>, to<Int>, [](const int &v){
-        return std::make_tuple(v - 1);
-      });
-
-  std::vector<std::tuple<Int, Double>> desired = {{{2},{5.}}, {{0},{2.}}};
-
-  test_ranges_equal(generated, desired);
+    // transform a single column to itself
+    std::vector<std::tuple<Int, Double>> data = {{{3}, {5.}}, {{1}, {2.}}};
+    auto generated = data
+      | transform(from<Int>, to<Int>, [](const int& v) { return std::make_tuple(v - 1); });
+    std::vector<std::tuple<Int, Double>> desired = {{{2}, {5.}}, {{0}, {2.}}};
+    test_ranges_equal(generated, desired);
 }
-
 
 BOOST_AUTO_TEST_CASE(test_move_only)
 {
-  // transform move-only column
-  std::vector<std::tuple<Int, Unique>> data;
-  data.emplace_back(3, std::make_unique<int>(5));
-  data.emplace_back(1, std::make_unique<int>(2));
+    // transform move-only column
+    std::vector<std::tuple<Int, Unique>> data;
+    data.emplace_back(3, std::make_unique<int>(5));
+    data.emplace_back(1, std::make_unique<int>(2));
 
-  auto generated =
-       data
-     | view::move
-     | transform(from<Unique>, to<Unique, Double>,
-         [](const std::unique_ptr<int> &ptr){
-           return std::make_tuple(std::make_unique<int>(*ptr), (double)*ptr);
-       })
-     | to_vector;
+    auto generated = data
+      | view::move
+      | transform(from<Unique>, to<Unique, Double>,
+          [](const std::unique_ptr<int> &ptr) {
+            return std::make_tuple(std::make_unique<int>(*ptr), (double)*ptr);
+        })
+      | to_vector;
 
-  // check unique pointers
-  std::vector<int> desired_ptr_vals{5, 2};
-  for (int i = 0; i < 2; ++i) {
-    BOOST_TEST(*(std::get<0>(generated[i]).value()[0]) == desired_ptr_vals[i]);
-  }
+    // check unique pointers
+    std::vector<int> desired_ptr_vals{5, 2};
+    for (int i = 0; i < 2; ++i) {
+        BOOST_TEST(*(std::get<0>(generated[i]).value()[0]) == desired_ptr_vals[i]);
+    }
 
-  // check other
-  auto to_check =
-      generated
-    | view::move
-    | drop<Unique>;
-
-  std::vector<std::tuple<Double, Int>> desired = {{5., 3}, {2., 1}};
-  test_ranges_equal(to_check, desired);
+    // check other
+    auto to_check = generated | view::move | drop<Unique>;
+    std::vector<std::tuple<Double, Int>> desired = {{5., 3}, {2., 1}};
+    test_ranges_equal(to_check, desired);
 }
-
 
 BOOST_AUTO_TEST_CASE(test_two_to_one)
 {
-  // transform two columns to a single column
-  std::vector<std::tuple<Int, Double>> data = {{{3},{5.}}, {{1},{2.}}};
-
-  auto generated =
-      data
-    | transform(from<Int, Double>, to<Double>, [](int i, double d){
-        return std::make_tuple((double)(i + d));
-      });
-
-  std::vector<std::tuple<Double, Int>> desired = {{{3 + 5.}, {3}}, {{1 + 2.}, {1}}};
-  test_ranges_equal(generated, desired);
+    // transform two columns to a single column
+    std::vector<std::tuple<Int, Double>> data = {{{3},{5.}}, {{1},{2.}}};
+  
+    auto generated = data
+      | transform(from<Int, Double>, to<Double>, [](int i, double d) {
+            return std::make_tuple((double)(i + d));
+        });
+  
+    std::vector<std::tuple<Double, Int>> desired = {{{3 + 5.}, {3}}, {{1 + 2.}, {1}}};
+    test_ranges_equal(generated, desired);
 }
-
 
 BOOST_AUTO_TEST_CASE(test_one_to_two)
 {
-  // transform a single column to two columns
-  std::vector<std::tuple<Int>> data = {{{3}}, {{1}}};
-
-  auto generated =
-      data
-    | transform(from<Int>, to<Int, Double>, [](int i){
-        return std::make_tuple(i + i, (double)(i * i));
-      });
-
-  std::vector<std::tuple<Int, Double>> desired = {{{6}, {9.}}, {{2}, {1.}}};
-  test_ranges_equal(generated, desired);
+    // transform a single column to two columns
+    std::vector<std::tuple<Int>> data = {{{3}}, {{1}}};
+  
+    auto generated = data
+      | transform(from<Int>, to<Int, Double>, [](int i) {
+            return std::make_tuple(i + i, (double)(i * i));
+        });
+  
+    std::vector<std::tuple<Int, Double>> desired = {{{6}, {9.}}, {{2}, {1.}}};
+    test_ranges_equal(generated, desired);
 }
