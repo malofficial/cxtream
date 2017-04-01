a03e7373a8cab18c1ee5055004e3920f7938efc1
Filip Matzner <floop@floop.cz>
2017-04-01 18:41:34 +0200

    Cleanup pyboost_cv_converter.hpp

--- src/python/utility/pyboost_cv2_converter.cpp
+++ src/python/utility/pyboost_cv2_converter.cpp
@@ -50,6 +50,35 @@ static PyObject* failmsgp(const char* fmt, ...)
 }
 */
 
+//===================    MACROS    =================================================================
+
+#define ERRWRAP2(expr) \
+try \
+{ \
+    PyAllowThreads allowThreads; \
+    expr; \
+} \
+catch (const cv::Exception &e) \
+{ \
+    PyErr_SetString(opencv_error, e.what()); \
+    return 0; \
+}
+
+//===================   UTILITY   ==================================================================
+
+static size_t REFCOUNT_OFFSET = (size_t)&(((PyObject*)0)->ob_refcnt) +
+  (0x12345678 != *(const size_t*)"\x78\x56\x34\x12\0\0\0\0\0")*sizeof(int);
+
+static inline PyObject* pyObjectFromRefcount(const int* refcount)
+{
+    return (PyObject*)((size_t)refcount - REFCOUNT_OFFSET);
+}
+
+static inline int* refcountFromPyObject(const PyObject* obj)
+{
+    return (int*)((size_t)obj + REFCOUNT_OFFSET);
+}
+
 //===================   THREADING     ==============================================================
 
 class PyAllowThreads {
