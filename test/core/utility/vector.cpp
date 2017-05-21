f0f80159ccfcc68a97fe97312b522ab25ead08bc
Filip Matzner <floop@floop.cz>
2017-05-21 03:15:59 +0200

    Add cutoff support for ndim_type and flat_view vector utils

--- test/core/utility/vector.cpp
+++ test/core/utility/vector.cpp
@@ -44,6 +44,16 @@ BOOST_AUTO_TEST_CASE(test_ndim_type)
       ndim_type<std::vector<std::vector<std::vector<double>>>>::type>{});
 }
 
+BOOST_AUTO_TEST_CASE(test_ndim_type_cutoff)
+{
+    static_assert(std::is_same<std::vector<std::vector<char>>,
+      ndim_type<std::vector<std::vector<char>>, 0>::type>{});
+    static_assert(std::is_same<std::vector<char>,
+      ndim_type<std::vector<std::vector<char>>, 1>::type>{});
+    static_assert(std::is_same<std::vector<double>,
+      ndim_type<std::vector<std::vector<std::vector<double>>>, 2>::type>{});
+}
+
 BOOST_AUTO_TEST_CASE(test_shape)
 {
     const std::vector<int> vec5 = {0, 0, 0, 0, 0};
@@ -120,6 +130,29 @@ BOOST_AUTO_TEST_CASE(test_flatten_identity)
 {
     const std::vector<int> vec = view::iota(1, 16);
     test_ranges_equal(flat_view(vec), view::iota(1, 16));
+}
+
+BOOST_AUTO_TEST_CASE(test_flatten_cutoff_dim1)
+{
+    const std::vector<std::vector<std::vector<int>>> vec = {
+        {{1, 2}, {3, 4}, {5}},
+        {{6}, {7, 8}}
+    };
+    std::vector<std::vector<std::vector<int>>> retrieved = flat_view<1>(vec);
+    BOOST_CHECK(retrieved == vec);
+}
+
+BOOST_AUTO_TEST_CASE(test_flatten_cutoff_dim2)
+{
+    const std::vector<std::vector<std::vector<int>>> vec = {
+        {{1, 2}, {3, 4}, {5}},
+        {{6}, {7, 8}}
+    };
+    const std::vector<std::vector<int>> desired = {
+        {{1, 2}, {3, 4}, {5}, {6}, {7, 8}}
+    };
+    std::vector<std::vector<int>> retrieved = flat_view<2>(vec);
+    BOOST_CHECK(retrieved == desired);
 }
 
 BOOST_AUTO_TEST_CASE(test_flatten_empty)
