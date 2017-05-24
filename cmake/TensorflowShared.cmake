80121afc71ea3bb1cd578c8892c161fa1769fd6f
Filip Matzner <floop@floop.cz>
2017-05-24 14:49:20 +0200

    Update built-in tensorflow version

--- cmake/TensorflowShared.cmake
+++ cmake/TensorflowShared.cmake
@@ -4,8 +4,7 @@ ExternalProject_Add(
   tensorflow_shared
   PREFIX third_party
   GIT_REPOSITORY http://github.com/tensorflow/tensorflow.git
-  # GIT_TAG v1.1.0-rc1
-  GIT_TAG 163da25caf2b63ba61fe94821de65c5362249691
+  GIT_TAG v1.2.0-rc0
   TMP_DIR "/tmp"
   STAMP_DIR "third_party/tensorflow-stamp"
   DOWNLOAD_DIR "third_party/tensorflow"
--- cmake/build_tensorflow.sh
+++ cmake/build_tensorflow.sh
@@ -26,6 +26,7 @@ export TF_NEED_HDFS=0
 export TF_NEED_OPENCL=0
 export TF_NEED_JEMALLOC=1
 export TF_NEED_VERBS=0
+export TF_NEED_MKL=0
 export TF_ENABLE_XLA=1
 export TF_CUDA_CLANG=0
 export PYTHON_BIN_PATH=/usr/bin/python
