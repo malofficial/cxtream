2a128165499589dc14b3629e55331eeb0d192f2e
Filip Matzner <floop@floop.cz>
2017-09-06 22:35:53 +0200

    Extend tests for pyboost_column_converter

--- test/python/utility/pyboost_column_converter.py
+++ test/python/utility/pyboost_column_converter.py
@@ -7,10 +7,30 @@ import pyboost_column_converter_py_cpp as pycpp
 def main():
     assert(np.array_equal(pycpp.py_vector1d_empty(), []))
 
-    # test whole range
-    assert(np.array_equal(pycpp.py_vector1d(), [1, 2, 3]))
+    # Test conversion of 1d, 2d and 3d vectors
+    # only the last dimension (i.e., the innermost vector)
+    # shall be a np.ndarray. All the other dimensions
+    # shall be pure pyhton lists.
+
+    # test whole range - 1d
+    assert(isinstance(pycpp.py_vector1d(), np.ndarray))
     assert(pycpp.py_vector1d().dtype == np.int32)
+    assert(np.array_equal(pycpp.py_vector1d(), [1, 2, 3]))
+
+    # test whole range - 2d
+    assert(isinstance(pycpp.py_vector2d(), list))
+    for arr in pycpp.py_vector2d():
+        assert(isinstance(arr, np.ndarray))
+        assert(arr.dtype == np.int32)
     assert(np.array_equal(pycpp.py_vector2d(), [[1, 2, 3]] * 3))
+
+    # test whole range - 3d
+    assert(isinstance(pycpp.py_vector3d(), list))
+    for d1_list in pycpp.py_vector3d():
+        assert(isinstance(d1_list, list))
+        for d2_arr in d1_list:
+            assert(isinstance(d2_arr, np.ndarray))
+            assert(d2_arr.dtype == np.int32)
     assert(np.array_equal(pycpp.py_vector3d(), [[[1, 2, 3]] * 3] * 3))
 
     # test slicing
