bcae235fcae2b623362b74c292938fe6c1fe9c5b
Filip Matzner <floop@floop.cz>
2017-09-27 04:19:42 +0200

    Do not remove GitHub's CNAME file

--- doc/clean-gh-pages.cmake
+++ doc/clean-gh-pages.cmake
@@ -1,6 +1,8 @@
 # remove all files except for the hidden ones
-file(GLOB _gh_files "[^.]*")
+file(GLOB _gh_files RELATIVE "${CMAKE_CURRENT_LIST_DIR}/gh-pages" "[^.]*")
 if(_gh_files)
+  # do not remove GitHub's CNAME file
+  list(REMOVE_ITEM _gh_files "CNAME")
   execute_process(COMMAND "${CMAKE_COMMAND}" -E remove ${_gh_files})
   foreach(_file ${_gh_files})
     execute_process(COMMAND "${CMAKE_COMMAND}" -E remove_directory ${_file})
