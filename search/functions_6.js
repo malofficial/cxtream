5fbe8244bb66c7304ab4f44ecc8e8ac42f0f5fc1
Filip Matzner <floop@floop.cz>
2018-01-26 11:40:14 +0100

    Update docs (commit 98784cc)

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['generate',['generate',['../group___stream.html#gafcc1b1ecdfc142625cd32cb6cc689c3f',1,'cxtream::stream::generate()'],['../group___vector.html#ga381bdd92eac59f5c4e89eddabc607bfb',1,'generate():&#160;vector.hpp']]],
   ['generate_5fgroups',['generate_groups',['../group___groups.html#gaecc4f370695f1d116136ddcf7b6890a8',1,'cxtream::generate_groups(std::size_t size, std::vector&lt; double &gt; ratio, Prng &amp;&amp;gen=utility::random_generator)'],['../group___groups.html#ga9bd077a33a06f6883c64f17ea1bd34dd',1,'cxtream::generate_groups(std::size_t n, std::size_t size, const std::vector&lt; double &gt; &amp;volatile_ratio, const std::vector&lt; double &gt; &amp;fixed_ratio, Prng &amp;&amp;gen=utility::random_generator)']]]
 ];
